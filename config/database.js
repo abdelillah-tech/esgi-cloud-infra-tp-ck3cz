0f9b35fc7ca9d53b91c784715305fea6da064302
Abdelillah GHOMARI <aghomari@myges.fr>
2021-02-09 13:22:18 +0100

    modify database.js file

--- config/database.js
+++ config/database.js
@@ -4,11 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: 'ec2-34-247-118-233.eu-west-1.compute.amazonaws.com',
+        port: '5432',
+        database: 'd4vesib4rfujc2',
+        username: 'ggjkpmleygmitc',
+        password: '020f7fce7e15ddfd8d5fae82ef3eb70bcfa08580fac31cb501ae9c6179c6aabb',
       },
       options: {
-        useNullAsDefault: true,
+        ssl: false,
       },
     },
   },
